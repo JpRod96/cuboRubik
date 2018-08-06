18dc709c72fa90f3e75f5ff0a6488fd66249df3d
jprodriguez60@gmail.com <jprodriguez60@gmail.com>
2018-08-06 18:11:32 -0400

    Added colors for the cube

--- src/Cube.java
+++ src/Cube.java
@@ -2,4 +2,11 @@
  * Created by Jp on 06/08/2018.
  */
 public class Cube {
+    private final char RED='r';
+    private final char BLUE='b';
+    private final char GREEN='g';
+    private final char ORANGE='o';
+    private final char WHITE='w';
+    private final char YELLOW='y';
+    private final int CUBE_SIZE =3;
 }
